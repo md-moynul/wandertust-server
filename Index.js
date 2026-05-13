0663c2e2eab4fe6a0951171652b214094340c8e4
md-moynul <mdmoynulislam58995@gmail.com>
2026-05-13 19:03:27 +0600

    feat: add API endpoints and middleware for destination management

--- Index.js
+++ Index.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const dotenv = require("dotenv")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require("cors")
 const app = express()
 dotenv.config()
 
 const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,14 +23,34 @@ async function run() {
     await client.connect();
     const db = client.db('wandertust')
     const destinationCollection = db.collection('destination')
-    app.post('/destination' , async(req , res) =>{
-        const newDestination = req.body;
-       const result = await destinationCollection.insertOne(newDestination)
-       res.json(result)
+    app.post('/destination', async (req, res) => {
+      const newDestination = req.body;
+      const result = await destinationCollection.insertOne(newDestination)
+      res.json(result)
     })
-
-
-
+    app.get('/destination', async (req, res) => {
+      const result = await destinationCollection.find().toArray()
+      res.send(result)
+    })
+    app.get('/destination/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await destinationCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+    app.patch('/destination/:id', async(req, res) => {
+       const id = req.params.id;
+       const updatedDestination = req.body;
+      const result = await destinationCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {$set :updatedDestination}
+      )
+      res.send(result)
+    })
+    app.delete('/destination/:id', async(req, res) => {
+       const id = req.params.id;
+      const result = await destinationCollection.deleteOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
 
 
 
@@ -35,14 +58,14 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
 
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-app.get('/' ,(req ,res) => {
-    res.send("hello world")
+app.get('/', (req, res) => {
+  res.send("hello world")
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
